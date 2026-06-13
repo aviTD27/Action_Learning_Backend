3c4c160959a33f08e1b09540573b87e05ef3471f
aviTD27 <avi.doorga27@gmail.com>
2026-06-13 18:03:44 +0200

    Feat: Update Main for Reminder Scheduler

--- src/main/java/fr/epita/Main.java
+++ src/main/java/fr/epita/Main.java
@@ -2,10 +2,12 @@ package fr.epita;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@EnableScheduling
 public class Main {
     public static void main(String[] args) {
 
